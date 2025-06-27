86475ab6197d5b2d7f38303f07a0d0f283bacd48
constv0 <2651219020@qq.com>
2025-06-27 14:18:52 +0800

    还原ksu

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -599,9 +599,7 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_devpts(struct inode*);
-#endif
+
 
 
 
@@ -613,9 +611,7 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU
-ksu_handle_devpts(dentry->d_inode);
-#endif
+
 	
 	
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
